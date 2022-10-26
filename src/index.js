df576e8e16d1d15cd8f2f8abd749d23a3977439b
Merrick <palmettobling@gmail.com>
2022-10-26 17:27:16 -0400

    Working on the Fetch

--- src/index.js
+++ src/index.js
@@ -3,21 +3,45 @@ var _height = 707;
 
 function drawImage() {
     var canvas = document.getElementById("ambassadorsUpcoming");
+    
+    // Today's date Header
+    let dateOptions = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        //timeZone: 'America/Los_Angeles',
+        //timeZoneName: 'short'
+    };
+    let today = new Date();
+    let intlToday = new Intl.DateTimeFormat('en', dateOptions).format(today);
 
+    // getting the schedule
+    //let response = fetch('https://www.xboxplaydates.us/api//ambassadorssplashimage');
+    //let schedule = response.text();
+    //console.log(schedule);
+    //console.log(JSON.stringify(schedule));
+
+    // drawing the canvas
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
 
         var img = new Image();
         img.src = `./img/static_background.png`;
         var boxart = new Image();
-        boxart.src = 'https://static-cdn.jtvnw.net/ttv-boxart/Fortnite-200x300.jpg';
+        //boxart.src = 'https://static-cdn.jtvnw.net/ttv-boxart/Fortnite-200x300.jpg';
         img.onload = function() {
+            fetch('https://www.xboxplaydates.us/api//ambassadorssplashimage')
+            .then(response => {
+                console.log("Then response json: ");
+                console.log(response.json());
+            })
             ctx.drawImage(img, 0, 0, _width, _height);
             ctx.fillStyle = 'white';
 
             ctx.font = "bold 54px 'Segoe UI',Arial,sans-serif"
             ctx.textAlign = 'center';
-            ctx.fillText("Time zones are dumb!", 611, 80);
+            ctx.fillText(intlToday, 711, 80);
 
             // get the number of shows for the day, divide the width by number of shows + 1, for center
             // boxart, number above offset by half the size of the image
@@ -26,45 +50,51 @@ function drawImage() {
             var showSpacingWidth = 1222 / (numberOfShows + 1);
             var x = showSpacingWidth;
             var y = 200;
-
+            
             ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
-            ctx.fillText('Show Info #1', x, y); // x, y
+            ctx.fillText(schedule[0].gameName, x, y); // x, y
 
+            boxArtURL = schedule[0].gameImage.replace("{width}x{height}", "200x300")
+            boxart.src = boxArtURL;
             ctx.drawImage(boxart, (x-100), (y+10)); // x-100, y+10
 
             ctx.font = "bold 20px 'Segoe UI'"
-            ctx.fillText('3:00 PM', x, (y + 340)) // x, y+340
+            ctx.fillText(schedule[0].showStartTime, x, (y + 340)) // x, y+340
 
             ctx.font = "bold 25px 'Segoe UI'"
-            ctx.fillText('Show #1 Name', x, (y+370)) //x, y+370
+            ctx.fillText(schedule[0].showName, x, (y+370)) //x, y+370
 
             //show 2
             ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
-            ctx.fillText('Show Info #2', (2 * x), y); // 2x, y
+            ctx.fillText(schedule[1].ganeName, (2 * x), y); // 2x, y
 
+            boxArtURL = schedule[1].gameImage.replace("{width}x{height}", "200x300")
+            boxart.src = boxArtURL;
             ctx.drawImage(boxart, ((2* x)-100), (y+10)); // 2x-100, y+10
             
             ctx.font = "bold 20px 'Segoe UI'"
-            ctx.fillText('3:00 PM', (2* x), (y + 340)) // 2x, y+340
+            ctx.fillText(schedule[1].showStartTime, (2* x), (y + 340)) // 2x, y+340
             
             ctx.font = "bold 25px 'Segoe UI'"
-            ctx.fillText('Show #1 Name', (2* x), (y+370)) //2x, y+370
+            ctx.fillText(schedule[1].showName, (2* x), (y+370)) //2x, y+370
 
             //show 3
             ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
-            ctx.fillText('Show Info #3', (3* x), y); // 3x, y
+            ctx.fillText(schedule[2].gameName, (3* x), y); // 3x, y
 
+            boxArtURL = schedule[2].gameImage.replace("{width}x{height}", "200x300")
+            boxart.src = boxArtURL;
             ctx.drawImage(boxart, ((3* x)-100), (y+10)); // 3x-100, y+10
 
             ctx.font = "bold 20px 'Segoe UI'"
-            ctx.fillText('3:00 PM', (3* x), (y + 340)) // 3x, y+340
+            ctx.fillText(schedule[2].showStartTime, (3* x), (y + 340)) // 3x, y+340
             
             ctx.font = "bold 25px 'Segoe UI'"
-            ctx.fillText('Show #1 Name', (3* x), (y+370)) //3x, y+370
+            ctx.fillText(schedule[1].showName, (3* x), (y+370)) //3x, y+370
             //end of show cards
-            
+
             ctx.font = "italic 38px 'Segoe UI',Arial,sans-serif"
-            ctx.fillText("Twitch.TV/XboxAmbassadors", 611, 650)
+            ctx.fillText("Twitch.TV/XboxAmbassadors", 611, 650);
         } 
     } else {
         alert("Something went wrong");
