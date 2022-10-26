77b5dc5b3500bedb38c5970143faac7503b8ae17
Merrick <palmettobling@gmail.com>
2022-10-26 08:52:19 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function drawImage() {
+    var canvas = document.getElementById("ambassadorsUpcoming");
+
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        var img = new Image();
+        img.src = `/src/img/static_background.png`;
+        img.onload = function() {
+            ctx.drawImage(img, 0, 0, 1222, 707);
+            
+        } 
+    } else {
+        alert("Something went wrong");
+    }
+}
