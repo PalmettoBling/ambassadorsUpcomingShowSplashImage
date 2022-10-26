f7faae011f2435a51357b00b2d821dbf525f96ef
Merrick <palmettobling@gmail.com>
2022-10-26 17:39:02 -0400

    Have data, need to work on formatting and box images

--- src/index.js
+++ src/index.js
@@ -32,69 +32,71 @@ function drawImage() {
         //boxart.src = 'https://static-cdn.jtvnw.net/ttv-boxart/Fortnite-200x300.jpg';
         img.onload = function() {
             fetch('https://www.xboxplaydates.us/api//ambassadorssplashimage')
-            .then(response => {
-                console.log("Then response json: ");
-                console.log(response.json());
+            .then((response) => response.json())
+            .then((data) => {
+                ctx.drawImage(img, 0, 0, _width, _height);
+                ctx.fillStyle = 'white';
+    
+                ctx.font = "bold 54px 'Segoe UI',Arial,sans-serif"
+                ctx.textAlign = 'center';
+                ctx.fillText(intlToday, 711, 80);
+    
+                // get the number of shows for the day, divide the width by number of shows + 1, for center
+                // boxart, number above offset by half the size of the image
+                // time is height  + boxart size with padding
+                var numberOfShows = 3;
+                var showSpacingWidth = 1222 / (numberOfShows + 1);
+                var x = showSpacingWidth;
+                var y = 200;
+                
+                ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
+                ctx.fillText(data[0].gameName, x, y); // x, y
+    
+                boxArtURL = data[0].gameImage.replace("{width}x{height}", "200x300")
+                boxart.src = boxArtURL;
+                ctx.drawImage(boxart, (x-100), (y+10)); // x-100, y+10
+    
+                ctx.font = "bold 20px 'Segoe UI'"
+                ctx.fillText(data[0].showStartTime, x, (y + 340)) // x, y+340
+    
+                ctx.font = "bold 25px 'Segoe UI'"
+                ctx.fillText(data[0].showName, x, (y+370)) //x, y+370
+    
+                //show 2
+                ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
+                ctx.fillText(data[1].ganeName, (2 * x), y); // 2x, y
+    
+                boxArtURL = data[1].gameImage.replace("{width}x{height}", "200x300")
+                boxart.src = boxArtURL;
+                ctx.drawImage(boxart, ((2* x)-100), (y+10)); // 2x-100, y+10
+                
+                ctx.font = "bold 20px 'Segoe UI'"
+                ctx.fillText(data[1].showStartTime, (2* x), (y + 340)) // 2x, y+340
+                
+                ctx.font = "bold 25px 'Segoe UI'"
+                ctx.fillText(data[1].showName, (2* x), (y+370)) //2x, y+370
+    
+                //show 3
+                ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
+                ctx.fillText(data[2].gameName, (3* x), y); // 3x, y
+    
+                boxArtURL = data[2].gameImage.replace("{width}x{height}", "200x300")
+                boxart.src = boxArtURL;
+                ctx.drawImage(boxart, ((3* x)-100), (y+10)); // 3x-100, y+10
+    
+                ctx.font = "bold 20px 'Segoe UI'"
+                ctx.fillText(data[2].showStartTime, (3* x), (y + 340)) // 3x, y+340
+                
+                ctx.font = "bold 25px 'Segoe UI'"
+                ctx.fillText(data[2].showName, (3* x), (y+370)) //3x, y+370
+                //end of show cards
+    
+                ctx.font = "italic 38px 'Segoe UI',Arial,sans-serif"
+                ctx.fillText("Twitch.TV/XboxAmbassadors", 611, 650);
             })
-            ctx.drawImage(img, 0, 0, _width, _height);
-            ctx.fillStyle = 'white';
-
-            ctx.font = "bold 54px 'Segoe UI',Arial,sans-serif"
-            ctx.textAlign = 'center';
-            ctx.fillText(intlToday, 711, 80);
-
-            // get the number of shows for the day, divide the width by number of shows + 1, for center
-            // boxart, number above offset by half the size of the image
-            // time is height  + boxart size with padding
-            var numberOfShows = 3;
-            var showSpacingWidth = 1222 / (numberOfShows + 1);
-            var x = showSpacingWidth;
-            var y = 200;
-            
-            ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
-            ctx.fillText(schedule[0].gameName, x, y); // x, y
-
-            boxArtURL = schedule[0].gameImage.replace("{width}x{height}", "200x300")
-            boxart.src = boxArtURL;
-            ctx.drawImage(boxart, (x-100), (y+10)); // x-100, y+10
-
-            ctx.font = "bold 20px 'Segoe UI'"
-            ctx.fillText(schedule[0].showStartTime, x, (y + 340)) // x, y+340
-
-            ctx.font = "bold 25px 'Segoe UI'"
-            ctx.fillText(schedule[0].showName, x, (y+370)) //x, y+370
-
-            //show 2
-            ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
-            ctx.fillText(schedule[1].ganeName, (2 * x), y); // 2x, y
-
-            boxArtURL = schedule[1].gameImage.replace("{width}x{height}", "200x300")
-            boxart.src = boxArtURL;
-            ctx.drawImage(boxart, ((2* x)-100), (y+10)); // 2x-100, y+10
-            
-            ctx.font = "bold 20px 'Segoe UI'"
-            ctx.fillText(schedule[1].showStartTime, (2* x), (y + 340)) // 2x, y+340
-            
-            ctx.font = "bold 25px 'Segoe UI'"
-            ctx.fillText(schedule[1].showName, (2* x), (y+370)) //2x, y+370
-
-            //show 3
-            ctx.font = "bolder italic 34px 'Segoe UI',Arial,sans-serif";
-            ctx.fillText(schedule[2].gameName, (3* x), y); // 3x, y
-
-            boxArtURL = schedule[2].gameImage.replace("{width}x{height}", "200x300")
-            boxart.src = boxArtURL;
-            ctx.drawImage(boxart, ((3* x)-100), (y+10)); // 3x-100, y+10
-
-            ctx.font = "bold 20px 'Segoe UI'"
-            ctx.fillText(schedule[2].showStartTime, (3* x), (y + 340)) // 3x, y+340
-            
-            ctx.font = "bold 25px 'Segoe UI'"
-            ctx.fillText(schedule[1].showName, (3* x), (y+370)) //3x, y+370
-            //end of show cards
-
-            ctx.font = "italic 38px 'Segoe UI',Arial,sans-serif"
-            ctx.fillText("Twitch.TV/XboxAmbassadors", 611, 650);
+            .catch(function(error) {
+                console.log(error)
+            });
         } 
     } else {
         alert("Something went wrong");
